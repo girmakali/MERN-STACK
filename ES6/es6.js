a9a60a35ad4c211c9df0fcc4a977be716bc5f43b
girmakali <mogesgirma44@yahoo.com>
2024-01-08 05:31:54 -0800

    fuctory-functions

--- ES6/es6.js
+++ ES6/es6.js
@@ -22,3 +22,39 @@ human.walk()
 
 const walk = human.walk;
 walk()
+
+
+//bind methods stick to the object in it
+const wal = human.walk.bind(human);
+walk()
+wal()
+
+//factory function uses for assigning arrays with symillar methods easily 
+function createCircle(radius){
+  return{
+    radius,
+    draw(){
+      console.log('draw')
+    },
+    area(){
+      console.log(3.14 * radius)
+    }
+  }
+}
+const circle1 = createCircle(1)
+console.log(circle1)
+circle1.draw()
+circle1.area()
+
+const circle2 =createCircle(3)
+console.log(circle2)
+circle2.area()
+
+
+
+
+
+
+
+
+
